34abeae63dba984bef35a7294d6acdac20b7ca71
PreetiMoo <preetissmoolya15@gmail.com>
2024-12-09 16:15:13 +0530

    FEurl

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postRoutes = require('./routes/postRoutes');
 const authorRoutes = require('./routes/authorRoutes');
+const helmet = require("helmet");
 require('dotenv').config();
 
 const app = express();
@@ -17,6 +18,22 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log('Error connecting to MongoDB', error));
 
+
+app.use(cors({
+  origin: ['https://blog-post-fe-psi.vercel.app/', 'http://localhost:3000'],
+  credentials: true
+}));
+
+
+app.use(helmet());
+
+
+const rateLimit = require('express-rate-limit');
+app.use(rateLimit({
+  windowMs: 15 * 60 * 10000, 
+  max: 10000 
+}));
+
 app.use('/api', postRoutes);
 app.use('/api', authorRoutes)
 
@@ -24,3 +41,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
